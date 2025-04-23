a1b355f4d2376af64841faa597928233f5ef20f3
bkimwoern <beatrice.kimwoern@student.unimelb.edu.au>
2025-04-23 20:09:31 +1000

    extracted injury levels from person_csv

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,15 @@
 import pandas as pd
 
 def preprocessing():
-    accident_100_csv = pd.read_csv('datasets/datasets-100/accident-100.csv')
-    print(accident_100_csv.head(100))
+    person_csv = pd.read_csv('datasets/datasets-100/person-100.csv')
+
+    fatalities_csv = person_csv[person_csv['INJ_LEVEL'] == 1]
+    serious_injury_csv = person_csv[person_csv['INJ_LEVEL'] == 2]
+    other_injury_csv = person_csv[person_csv['INJ_LEVEL'] == 3]
+    not_injured_csv = person_csv[person_csv['INJ_LEVEL'] == 4]
+
+    #print(fatalities_csv.head(100))
+    #print(serious_injury_csv.head(100))
+    #print(other_injury_csv.head(100))
+    #print(not_injured_csv.head(100))
 
