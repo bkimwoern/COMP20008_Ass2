f8aa8fed774720cd0f3b9d04b14398dc9d25e004
bkimwoern <beatrice.kimwoern@student.unimelb.edu.au>
2025-04-25 15:13:22 +1000

    Started preprocessing for person csv. Imputed blank values in SEATING_POSITION, and categorised people who were in an enclosed vehicle

--- preprocessing.py
+++ preprocessing.py
@@ -1,40 +1,33 @@
+import numpy as np
 import pandas as pd
 
 def preprocessing():
     """Pre-processing person_csv"""
     person_csv = pd.read_csv('datasets/datasets-100/person-100.csv')
+    process_person_csv(person_csv)
 
-    # replacing the previous age column with new one containing broader ranges
-    # dropping 'Unknown' age group, as only makes up 3% of dataset.
-    age_group = person_csv['AGE_GROUP'].map(categorise_age)
-    person_csv['AGE_GROUP'] = age_group
-
-    print(age_group)
-
-    fatalities_csv = filter_out_value(person_csv, 'INJ_LEVEL', 1)
-    serious_injury_csv = filter_out_value(person_csv, 'INJ_LEVEL', 2)
-    other_injury_csv = filter_out_value(person_csv, 'INJ_LEVEL', 3)
-    not_injured_csv = filter_out_value(person_csv, 'INJ_LEVEL', 4)
-
-    #print(fatalities_csv.head(100))
-    #print(serious_injury_csv.head(100))
-    #print(other_injury_csv.head(100))
-    #print(not_injured_csv.head(100))
-
-""" From task 1_1, assignment 1"""
-def categorise_age(age_group):
-    """ Categorises age groups into broader ranges"""
-    if age_group in ['13-15']:
-        return 'Under 16'
-    elif age_group in ['16-17', '18-21', '22-25']:
-        return '16-25'
-    elif age_group in ['26-29', '30-39']:
-        return '26-39'
-    elif age_group in ['40-49', '50-59', '60-64']:
-        return '40-64'
-    elif age_group in ['65-69', '70+']:
-        return '65+'
+    accident_csv = pd.read_csv('datasets/datasets-100/accident-100.csv')
 
 def filter_out_value(record, column, value):
     """ Filters a DataFrame based on a given column-value pair """
     return record[record[column] == value]
+
+def process_person_csv(person_csv):
+    filtered_person = person_csv
+
+    # --- Imputing blanks in SEATING_POSITION ---
+    # The only blanks in SEATING_POSITION are for 'drivers' in ROAD_USER_TYPE
+    # Normalising blanks to nan values
+    filtered_person['SEATING_POSITION'].replace('', np.nan, inplace=True)
+    driver_mask = (filtered_person['SEATING_POSITION'].isna() &
+                   (filtered_person['ROAD_USER_TYPE_DESC'] == 'Drivers'))
+    # Imputing these rows to '1'
+    filtered_person.loc[driver_mask, 'SEATING_POSITION'] = '1'
+
+    # --- Creating new column indicating whether a person was in an enclosed vehicle ---
+    # Defaulting all values to 0 (is not in enclosed vehicle)
+    filtered_person['IN_METAL_BOX'] = 0
+    filtered_person.loc[filtered_person['ROAD_USER_TYPE_DESC'].isin(['Drivers', 'Passengers'])] = 1
+
+
+    filtered_person.to_csv('datasets/filtered_person.csv', index=False)
