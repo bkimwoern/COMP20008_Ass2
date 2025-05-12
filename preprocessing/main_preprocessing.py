10d70518d35c3bc3a00b9b8be33a0d776f553dec
bkimwoern <beatrice.kimwoern@student.unimelb.edu.au>
2025-05-12 21:28:13 +1000

    sorry forgot to comment last file

--- preprocessing/main_preprocessing.py
+++ preprocessing/main_preprocessing.py
@@ -2,6 +2,7 @@ from .vehicle_processing import process_vehicle_csv, process_vehicle_body_type
 from .person_processing import process_person_csv
 from .accident_processing import process_accident_csv
 
+# --- Main preprocessing function that calls sub-functions to preprocess specific csv files ---
 def preprocessing():
     process_vehicle_csv()
     process_person_csv()
